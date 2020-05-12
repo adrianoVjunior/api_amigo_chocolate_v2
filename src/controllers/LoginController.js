9fb16b9e41cd8a9227bf0aaa5c0541d62005816d
Adriano de Paula Veríssimo Júnior <adriano.junior@zema.com>
2020-05-12 16:18:34 -0300

    first commit

---
+++ src/controllers/LoginController.js
@@ -0,0 +1,13 @@
+const Pessoa = require('../models/Pessoa');
+const jwt = require('jsonwebtoken')
+
+module.exports = {
+    async geraToken(request, response){
+
+        let { apelido, senha } = request.body;
+        const UsuarioRetorno = await Pessoa.findOne({ apelido : apelido, senha: senha});
+        // eslint-disable-next-line no-undef
+        const token = jwt.sign({_id: UsuarioRetorno._id, senha: UsuarioRetorno.senha}, process.env.JWT_KEY, { expiresIn: 1000 });
+        return response.send({auth : true,  token : token});
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes')
+
+const app = express()
+
+app.use(express.json())
+app.use(routes)
+
+// eslint-disable-next-line no-undef
+mongoose.connect(process.env.connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+
+app.listen(process.env.PORT || 3333, () => {
+    console.log('Aplicação rodando na porta 3333!')
+})
